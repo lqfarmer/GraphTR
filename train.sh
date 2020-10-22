9e35e3b33ea437cd033e302c04f215f67d3a848c
addisliu(刘祺) <addisliu@tencent.com>
2020-10-22 20:52:56 +0800

    d

---
+++ train.sh
@@ -0,0 +1,12 @@
+../../py27/bin/python -m gat.train \
+                  --epochs 10 \
+                  --batch_size 64 \
+                  --train_prefix ./ \
+                  --adj_name sample_adj \
+                  --model gat_multi \
+                  --result_type emb \
+                  --max_total_steps 100000000 \
+                  --sample_choice weight_sample \
+                  --validate_iter 50 \
+                  --save_per_epoch 10 \
+                  --dropout 0.3
